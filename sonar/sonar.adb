75bdccd543d32e07de647feae8d18304ea70115e
Przemyslaw Kobylanski <przemko@mac.com>
2017-04-07 13:02:55 +0200

    dodano projekt sonar

---
+++ sonar/sonar.adb
@@ -0,0 +1,58 @@
+with AVR.Real_Time.Delays;
+with AVR.Interrupts;
+with LiquidCrystal;
+
+with AVR; use AVR;
+with AVR.MCU;
+with AVR.Wait;
+
+procedure Sonar is
+   
+   Trigger    : Boolean renames MCU.PORTD_Bits (0);
+   Trigger_DD : Boolean renames MCU.DDRD_Bits  (0);
+   Echo_Pin   : Boolean renames MCU.PIND_Bits  (1);
+   Echo       : Boolean renames MCU.PORTD_Bits (1);
+   Echo_DD    : Boolean renames MCU.DDRD_Bits  (1);
+   
+   Processor_Speed : constant := 16_000_000;
+   
+   Counter : Integer;
+   
+   procedure Wait_2us is new AVR.Wait.Generic_Wait_USecs
+     (Crystal_Hertz => Processor_Speed,
+      Micro_Seconds => 2);
+   
+   procedure Wait_10us is new AVR.Wait.Generic_Wait_USecs
+     (Crystal_Hertz => Processor_Speed,
+      Micro_Seconds => 10);
+   
+begin
+   LiquidCrystal.Init (16, 2);
+   LiquidCrystal.Clear;
+   loop
+      Trigger_DD := DD_Output;
+      Trigger := Low;
+      Wait_2us;
+      Trigger := High;
+      Wait_10us;
+      Trigger := Low;
+      Echo_DD := DD_Input;
+      Echo := Low;
+      
+      Counter := 0;     
+      while Echo_Pin = Low loop
+	 Counter := Counter + 1;
+      end loop;
+      while not Echo_Pin = High loop
+	 Counter := Counter + 1;
+      end loop;
+      while Echo_Pin = High loop
+	 Counter := Counter + 1;
+      end loop;
+      
+      LiquidCrystal.Set_Cursor (0, 0);
+      LiquidCrystal.Put (Counter);
+      LiquidCrystal.Put ("     ");
+      delay 1.0;
+   end loop;
+end Sonar;
